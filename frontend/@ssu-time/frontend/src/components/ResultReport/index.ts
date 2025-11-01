c54da39448ebb82b2789e5ac0bff12a2fee06ef1
dwl21 <nggus5@gmail.com>
2025-11-01 17:41:20 +0900

    feat: reset ssu-time frontend scaffold

--- frontend/@ssu-time/frontend/src/components/ResultReport/index.ts
+++
@@ -1,8 +0,0 @@
-export { ResultReport } from './ResultReport';
-export { ResultReportOverlay } from './ResultReportOverlay';
-export { ResultReportContainer } from './ResultReportContainer';
-export { ResultReportHeader } from './ResultReportHeader';
-export { ResultReportContent } from './ResultReportContent';
-export { ResultReportFooter } from './ResultReportFooter';
-export { ResultReportSlideCard } from './ResultReportSlideCard';
-export type { ResultReportProps, PracticeResult } from './ResultReport';
