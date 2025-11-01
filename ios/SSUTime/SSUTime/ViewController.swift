8cb821eb80f788de065a8105cfd01a239f830ee0
hyeonju <shj040201@naver.com>
2025-11-01 16:05:23 +0900

    [Add] iOS 프로젝트 추가

---
+++ ios/SSUTime/SSUTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSUTime
+//
+//  Created by 성현주 on 11/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
