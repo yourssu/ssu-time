a4394270aa75927b0abb15194dcf7084d841bf8d
00yhsp <00yhsp@naver.com>
2025-11-01 16:49:21 +0900

    [feat] Widget Extension 추가

---
+++ ios/SSUTime/SSUTimeWidget/SSUTimeWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  SSUTimeWidgetBundle.swift
+//  SSUTimeWidget
+//
+//  Created by 박현수 on 11/1/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct SSUTimeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        SSUTimeWidget()
+        SSUTimeWidgetControl()
+    }
+}
