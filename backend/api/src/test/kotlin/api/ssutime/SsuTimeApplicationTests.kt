9f9ab2018cc654ad39cee60ef2159a230adeab87
leeminseo <leeminseo0923@gmail.com>
2025-11-30 03:09:45 +0900

    refactor: DemoApplicationTests를 SsuTimeApplicationTests.kt로 변경

--- backend/api/src/test/kotlin/api/ssutime/SsuTimeApplicationTests.kt
+++ backend/api/src/test/kotlin/api/ssutime/SsuTimeApplicationTests.kt
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class DemoApplicationTests {
+class SsuTimeApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+    }
 
 }
